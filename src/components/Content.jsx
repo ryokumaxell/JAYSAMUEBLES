1e74b242e27712d068f528bed0e281ac337cd284
JL Acosta_RyokuMaxell <ryoku.maxell11@gmail.com>
2023-03-30 13:34:54 +0000

    change

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,43 +1,5 @@
-export default function Content(props) {
+export default function Content() {
   return (
-    <section className=" relative md:px-20 max-w-screen-xl mx-auto">
-      
-
-      <div className="py-5 flex justify-between items-center">
-        <p className="font-rubik font-light text-6xl p-2 tracking-wider leading-relaxed uppercase">
-          {props.data.title}
-        </p>
-        <p className="font-rubik font-light text-base tracking-wider leading-6">
-          {props.data.description}
-        </p>
-      </div>
-
-      <div className="py-5">{props.data.image}</div>
-
-      
-
-      <div className="flex justify-between px-3 py-5">
-        <div className="w-1/3 relative">
-          {props.data.imagecontent[0]}
-          <p className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-white text-lg font-bold text-center">
-            SILLAS
-          </p>
-        </div>
-
-        <div className="w-1/3 relative">
-          {props.data.imagecontent[1]}
-          <p className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-white text-lg font-bold text-center">
-            ESCRITORIOS
-          </p>
-        </div>
-
-        <div className="w-1/3 relative">
-          {props.data.imagecontent[2]}
-          <p className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-white text-lg font-bold text-center">
-            SISTEMAS MODULARES
-          </p>
-        </div>
-      </div>
-    </section>
+    ""
   );
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,70 +1,66 @@
 import React from "react";
-import Content from "../components/Content";
-import Carrusel from "../components/Hero";
 
-const contentData = [
-  //primera seccion del contedido de la web
-  {
-    image: 
-      <img
-        src="https://images.squarespace-cdn.com/content/v1/54263706e4b0e2d40dd9f6d4/1662655441708-3N9AAX3DJR11TQ1KMS7C/DSC09779+Mesa+silla+verde+crop.jpg"
-        atl="Mesa silla verde crop"
-        title="Mesa silla verde crop"
-      />
-    ,
-    title: "LÍNEA OFFICE",
-    description:
-      "Mobiliario ergonómico de tendencia para espacios de trabajo,oficinas y ambientes ejecutivos de todo tipo. Variedad de sillas,mesas, escritorios, recepciones, sala de reuniones y espacios compartidos, todos pensados en la funcionalidad necesaria para los espacios corporativos actuales.",
-    imagecontent: [
-      <img
-        src="https://images.squarespace-cdn.com/content/v1/54263706e4b0e2d40dd9f6d4/1603938240291-QUTS561YKK05AVE04C79/black+mesh.jpg"
-        alt="SILLAS DE ESCRITORIO"
-      />,
-      <img
-        src="https://images.squarespace-cdn.com/content/v1/54263706e4b0e2d40dd9f6d4/1662574927773-OBTQ59SZMZBBZLSWVZLO/DSC00260.JPG"
-        alt="ESCRITORIOs"
-      />,
-      <img
-        src="https://images.squarespace-cdn.com/content/v1/54263706e4b0e2d40dd9f6d4/1662574747810-DVCK29S57X475CZR6JNR/DSC00276.jpg"
-        alt="SISTEMAS MODULARES"
-      />,
-    ],
-  },
-  //siguiente seccion
-  {
-    image: (
-      <img
-        src="https://images.squarespace-cdn.com/content/v1/54263706e4b0e2d40dd9f6d4/5b17c9b4-59cd-4345-8865-244ad5a9eea2/SKY+LOUNGE+crop.jpg"
-        atl="Mesa silla blanca ext"
-        title="Mesa silla blanca ext"
-      />
-    ),
-    title: "LÍNEA CONTRACT",
-    description:
-      "Amplia línea que incluye sillas, taburetes, tumbonas y mesas, todos diseñados para ser multifuncionales y con un estilo auténtico, moderno y novedoso.  Fabricados con materiales de tendencia como el policarbonato transparente, y resistentes a condiciones medioambientales usuales.",
-    imagecontent: [
-      <img
-        src="https://images.squarespace-cdn.com/content/v1/54263706e4b0e2d40dd9f6d4/1662576537788-68IGY6E1XXLARBTYMLAN/new_contract_gallery_498_air_ailesi%25402x.jpg"
-        alt="SILLAS"
-      />,
-      <img
-        src="https://images.squarespace-cdn.com/content/v1/54263706e4b0e2d40dd9f6d4/1662575980884-XGZW9YQDWUJ8RPQFUDBJ/500_air_air140.jpg"
-        alt="MESAS"
-      />,
-      <img
-        src="https://images.squarespace-cdn.com/content/v1/54263706e4b0e2d40dd9f6d4/1662576593646-DGT31UA0VYEY9RNOY0O6/image-small-1578294193-7068.jpg"
-        alt="TUMBONAS Y LOUNGES"
-      />,
-    ],
-  },
-];
+import Carrusel from "../components/Hero";
 
 export default function Home() {
   return (
     <>
-    <Carrusel/>
-      <Content data={contentData[0]} />
-      <Content data={contentData[1]} />
+      <Carrusel />
+      <section className=" relative md:px-20 max-w-screen-xl mx-auto">
+        <div className="py-5 flex justify-between items-center">
+          <p className="font-rubik font-light text-6xl p-2 tracking-wider leading-relaxed uppercase">
+             MODELOS OFICINA
+          </p>
+          <p className="font-rubik font-light p-2 text-base tracking-wider leading-6">
+            Mobiliario ergonómico de tendencia para espacios de trabajo,oficinas
+            y ambientes ejecutivos de todo tipo. Variedad de sillas,mesas,
+            escritorios, recepciones, sala de reuniones y espacios compartidos,
+            todos pensados en la funcionalidad necesaria para los espacios
+            corporativos actuales.
+          </p>
+        </div>
+
+        <div className="py-5">
+          <img
+            src="https://images.squarespace-cdn.com/content/v1/54263706e4b0e2d40dd9f6d4/1662655441708-3N9AAX3DJR11TQ1KMS7C/DSC09779+Mesa+silla+verde+crop.jpg"
+            atl="Mesa silla verde crop"
+            title="Mesa silla verde crop"
+          />
+        </div>
+
+        <div className="flex justify-between px-3 py-5">
+          <div className="w-1/3 relative">
+          <img
+        src="https://images.squarespace-cdn.com/content/v1/54263706e4b0e2d40dd9f6d4/1603938240291-QUTS561YKK05AVE04C79/black+mesh.jpg"
+        alt="SILLAS DE ESCRITORIO"
+      />
+            <p className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-white text-lg font-bold text-center">
+              SILLAS
+            </p>
+          </div>
+
+          <div className="w-1/3 relative">
+          <img
+        src="https://images.squarespace-cdn.com/content/v1/54263706e4b0e2d40dd9f6d4/1662574927773-OBTQ59SZMZBBZLSWVZLO/DSC00260.JPG"
+        alt="ESCRITORIOs"
+      />
+            <p className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-white text-lg font-bold text-center">
+              ESCRITORIOS
+            </p>
+          </div>
+
+          <div className="w-1/3 relative">
+          <img
+        src="https://images.squarespace-cdn.com/content/v1/54263706e4b0e2d40dd9f6d4/1662574747810-DVCK29S57X475CZR6JNR/DSC00276.jpg"
+        atl="Mesa silla blanca ext"
+        title="Mesa silla blanca ext"
+      />
+            <p className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-white text-lg font-bold text-center">
+              SISTEMAS MODULARES
+            </p>
+          </div>
+        </div>
+      </section>
     </>
   );
 }
